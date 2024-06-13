b5532d5f5e601e07ca73ff449ecd6049b8cb6cd9
masterOrigen <167365847+masterOrigen@users.noreply.github.com>
2024-06-13 18:11:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,19 @@ import streamlit as st
 from dotenv import load_dotenv
 from youtube_transcript_api import YouTubeTranscriptApi
 
-load_dotenv() ##load all the nevironment variables
+load_dotenv()  # Cargar todas las variables de entorno
 import os
 import google.generativeai as genai
 
+# Configurar la API de Gemini
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-prompt = """You are Yotube video summarizer. You will be taking the transcript text
+# Prompt para la generación de resúmenes
+prompt = """You are YouTube video summarizer. You will be taking the transcript text
 and summarizing the entire video and providing the important summary in points
 within 250 words. Please provide the summary of the text given here: """
 
-## getting the transcript data from yt videos
+# Obtener los detalles de la transcripción de los videos de YouTube
 def extract_transcript_details(youtube_video_url):
     try:
         video_id = youtube_video_url.split("=")[1]
@@ -28,7 +30,7 @@ def extract_transcript_details(youtube_video_url):
         st.error(f"Error al obtener la transcripción del video: {e}")
         st.stop()
 
-## getting the summary based on Prompt from Google Gemini Pro
+# Generar el contenido utilizando Gemini Pro
 def generate_gemini_content(transcript_text, prompt):
     model = genai.GenerativeModel("gemini-pro")
     response = model.generate_content(prompt + transcript_text)
