13c3f601f3ebf665d012e5a384c38c703e1e89fc
masterOrigen <167365847+masterOrigen@users.noreply.github.com>
2024-06-14 14:16:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,88 +1,277 @@
 import streamlit as st
-from dotenv import load_dotenv
-from youtube_transcript_api import YouTubeTranscriptApi
-import os
+import pandas as pd
 import google.generativeai as genai
-import spacy
+import re
+from PIL import Image
+import requests
 
-load_dotenv()  # Cargar todas las variables de entorno
+#Je t'aime plus que les mots,
+#Plus que les sentiments,
+#Plus que la vie elle-même
 
-# Configurar la API de Gemini
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+st.set_page_config(
+    page_title="Google AI Chat",
+    page_icon="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png",
+    layout="wide",
+)
+# Path: Main.py
+#Author: Sergio Demis Lopez Martinez
+#------------------------------------------------------------
+#HEADER
+st.markdown('''
+Powered by Google AI <img src="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png" width="20" height="20">
+, Streamlit and Python''', unsafe_allow_html=True)
+st.caption("By Sergio Demis Lopez Martinez")
 
-# Cargar el modelo de procesamiento de lenguaje natural (en este caso, spaCy en español)
-nlp = spacy.load("es_core_news_sm")
+#------------------------------------------------------------
+#LANGUAGE
+langcols = st.columns([0.2,0.8])
+with langcols[0]:
+  lang = st.selectbox('Select your language',
+  ('English', 'Español', 'Français', 'Deutsch',
+  'Italiano', 'Português', 'Polski', 'Nederlands',
+  'Русский', '日本語', '한국어', '中文', 'العربية',
+  'हिन्दी', 'Türkçe', 'Tiếng Việt', 'Bahasa Indonesia',
+  'ภาษาไทย', 'Română', 'Ελληνικά', 'Magyar', 'Čeština',
+  'Svenska', 'Norsk', 'Suomi', 'Dansk', 'हिन्दी', 'हिन्�'),index=1)
 
-# Prompt para la generación de resúmenes
-SUMMARY_PROMPT = """You are a YouTube video summarizer. You will take the transcript text
-and summarize the entire video, providing an important summary within 250 words.
-Please provide the summary of the text given here: """
+if 'lang' not in st.session_state:
+    st.session_state.lang = lang
+st.divider()
 
-# Obtener los detalles de la transcripción de los videos de YouTube
-def extract_transcript_details(video_url, language="es"):
-    try:
-        video_id = video_url.split("=")[1]
-        transcript_text = YouTubeTranscriptApi.get_transcript(video_id, languages=[language])
+#------------------------------------------------------------
+#FUNCTIONS
+def extract_graphviz_info(text: str) -> list[str]:
+  """
+  The function `extract_graphviz_info` takes in a text and returns a list of graphviz code blocks found in the text.
 
-        transcript = ""
-        for i in transcript_text:
-            transcript += " " + i["text"]
+  :param text: The `text` parameter is a string that contains the text from which you want to extract Graphviz information
+  :return: a list of strings that contain either the word "graph" or "digraph". These strings are extracted from the input
+  text.
+  """
 
-        return transcript
+  graphviz_info  = text.split('```')
 
-    except Exception as e:
-        st.warning(f"El video no tiene transcripción disponible: {e}")
-        st.stop()
+  return [graph for graph in graphviz_info if ('graph' in graph or 'digraph' in graph) and ('{' in graph and '}' in graph)]
 
-# Generar el contenido utilizando Gemini Pro
-def generate_gemini_content(transcript_text, prompt):
-    model = genai.GenerativeModel("gemini-pro")
-    response = model.generate_content(prompt + transcript_text)
-    return response.text
+def append_message(message: dict) -> None:
+    """
+    The function appends a message to a chat session.
 
-# Procesar la pregunta y obtener una respuesta basada en el contenido del video
-def process_question(question, transcript_text):
-    # Analizar la pregunta utilizando spaCy
-    doc = nlp(question)
-    
-    # Extraer entidades nombradas relevantes de la pregunta
-    relevant_entities = [ent.text for ent in doc.ents if ent.label_ in ["ORG", "PERSON", "WORK_OF_ART"]]
-    
-    # Realizar búsqueda de las entidades en la transcripción
-    relevant_sentences = []
-    for sentence in transcript_text.split("."):
-        for entity in relevant_entities:
-            if entity.lower() in sentence.lower():
-                relevant_sentences.append(sentence.strip())
-                break
-    
-    # Devolver la respuesta como un resumen de las oraciones relevantes
-    if relevant_sentences:
-        return ". ".join(relevant_sentences)
+    :param message: The `message` parameter is a dictionary that represents a chat message. It typically contains
+    information such as the user who sent the message and the content of the message
+    :type message: dict
+    :return: The function is not returning anything.
+    """
+    st.session_state.chat_session.append({'user': message})
+    return
+
+@st.cache_resource
+def load_model() -> genai.GenerativeModel:
+    """
+    The function `load_model()` returns an instance of the `genai.GenerativeModel` class initialized with the model name
+    'gemini-pro'.
+    :return: an instance of the `genai.GenerativeModel` class.
+    """
+    model = genai.GenerativeModel('gemini-pro')
+    return model
+
+@st.cache_resource
+def load_modelvision() -> genai.GenerativeModel:
+    """
+    The function `load_modelvision` loads a generative model for vision tasks using the `gemini-pro-vision` model.
+    :return: an instance of the `genai.GenerativeModel` class.
+    """
+    model = genai.GenerativeModel('gemini-pro-vision')
+    return model
+
+
+
+#------------------------------------------------------------
+#CONFIGURATION
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+
+model = load_model()
+
+vision = load_modelvision()
+
+if 'chat' not in st.session_state:
+    st.session_state.chat = model.start_chat(history=[])
+
+if 'chat_session' not in st.session_state:
+    st.session_state.chat_session = []
+
+#st.session_state.chat_session
+
+#------------------------------------------------------------
+#CHAT
+
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+if 'welcome' not in st.session_state or lang != st.session_state.lang:
+    st.session_state.lang = lang
+    welcome  = model.generate_content(f'''
+    Da un saludo de bienvenida al usuario y sugiere que puede hacer
+    (Puedes describir imágenes, responder preguntas, leer archivos texto, leer tablas,generar gráficos con graphviz, etc)
+    eres un chatbot en una aplicación de chat creada en streamlit y python. generate the answer in {lang}''')
+    welcome.resolve()
+    st.session_state.welcome = welcome
+
+    with st.chat_message('ai'):
+        st.write(st.session_state.welcome.text)
+else:
+    with st.chat_message('ai'):
+        st.write(st.session_state.welcome.text)
+
+if len(st.session_state.chat_session) > 0:
+    count = 0
+    for message in st.session_state.chat_session:
+
+        if message['user']['role'] == 'model':
+            with st.chat_message('ai'):
+                st.write(message['user']['parts'])
+                graphs = extract_graphviz_info(message['user']['parts'])
+                if len(graphs) > 0:
+                    for graph in graphs:
+                        st.graphviz_chart(graph,use_container_width=False)
+                        if lang == 'Español':
+                          view = "Ver texto"
+                        else:
+                          view = "View text"
+                        with st.expander(view):
+                          st.code(graph, language='dot')
+        else:
+            with st.chat_message('user'):
+                st.write(message['user']['parts'][0])
+                if len(message['user']['parts']) > 1:
+                    st.image(message['user']['parts'][1], width=200)
+        count += 1
+
+
+
+#st.session_state.chat.history
+
+cols=st.columns(4)
+
+with cols[0]:
+    if lang == 'Español':
+      image_atachment = st.toggle("Adjuntar imagen", value=False, help="Activa este modo para adjuntar una imagen y que el chatbot pueda leerla")
     else:
-        return "Lo siento, no pude encontrar información relevante en el video para responder a tu pregunta."
+      image_atachment = st.toggle("Attach image", value=False, help="Activate this mode to attach an image and let the chatbot read it")
 
-# Interfaz de usuario
-def main():
-    st.title("Transcripción de Videos")
-    video_url = st.text_input("Ingresa el enlace del video de YouTube:")
+with cols[1]:
+    if lang == 'Español':
+      txt_atachment = st.toggle("Adjuntar archivo de texto", value=False, help="Activa este modo para adjuntar un archivo de texto y que el chatbot pueda leerlo")
+    else:
+      txt_atachment = st.toggle("Attach text file", value=False, help="Activate this mode to attach a text file and let the chatbot read it")
+with cols[2]:
+    if lang == 'Español':
+      csv_excel_atachment = st.toggle("Adjuntar CSV o Excel", value=False, help="Activa este modo para adjuntar un archivo CSV o Excel y que el chatbot pueda leerlo")
+    else:
+      csv_excel_atachment = st.toggle("Attach CSV or Excel", value=False, help="Activate this mode to attach a CSV or Excel file and let the chatbot read it")
+with cols[3]:
+    if lang == 'Español':
+      graphviz_mode = st.toggle("Modo graphviz", value=False, help="Activa este modo para generar un grafo con graphviz en .dot a partir de tu mensaje")
+    else:
+      graphviz_mode = st.toggle("Graphviz mode", value=False, help="Activate this mode to generate a graph with graphviz in .dot from your message")
+if image_atachment:
+    if lang == 'Español':
+      image = st.file_uploader("Sube tu imagen", type=['png', 'jpg', 'jpeg'])
+      url = st.text_input("O pega la url de tu imagen")
+    else:
+      image = st.file_uploader("Upload your image", type=['png', 'jpg', 'jpeg'])
+      url = st.text_input("Or paste your image url")
+else:
+    image = None
+    url = ''
 
-    if video_url:
-        video_id = video_url.split("=")[1]
-        st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
 
-    if st.button("Procesar"):
-        transcript_text = extract_transcript_details(video_url)
 
-        if transcript_text:
-            st.markdown("## Transcripción:")
-            st.write(transcript_text)
+if txt_atachment:
+    if lang == 'Español':
+      txtattachment = st.file_uploader("Sube tu archivo de texto", type=['txt'])
+    else:
+      txtattachment = st.file_uploader("Upload your text file", type=['txt'])
+else:
+    txtattachment = None
 
-            user_question = st.text_area("Escribe tu pregunta:")
-            if st.button("Enviar pregunta"):
-                st.write("Tu Pregunta:", user_question)
-                answer = process_question(user_question, transcript_text)
-                st.write("Respuesta:", answer)
+if csv_excel_atachment:
+    if lang == 'Español':
+      csvexcelattachment = st.file_uploader("Sube tu archivo CSV o Excel", type=['csv', 'xlsx'])
+    else:
+      csvexcelattachment = st.file_uploader("Upload your CSV or Excel file", type=['csv', 'xlsx'])
+else:
+    csvexcelattachment = None
+if lang == 'Español':
+  prompt = st.chat_input("Escribe tu mensaje")
+else:
+  prompt = st.chat_input("Write your message")
 
-if __name__ == "__main__":
-    main()
+if prompt:
+    txt = ''
+    if txtattachment:
+        txt = txtattachment.getvalue().decode("utf-8")
+        if lang == 'Español':
+          txt = '   Archivo de texto: \n' + txt
+        else:
+          txt = '   Text file: \n' + txt
+
+    if csvexcelattachment:
+        try:
+            df = pd.read_csv(csvexcelattachment)
+        except:
+            df = pd.read_excel(csvexcelattachment)
+        txt += '   Dataframe: \n' + str(df)
+
+    if graphviz_mode:
+        if lang == 'Español':
+          txt += '   Genera un grafo con graphviz en .dot \n'
+        else:
+          txt += '   Generate a graph with graphviz in .dot \n'
+
+    if len(txt) > 5000:
+        txt = txt[:5000] + '...'
+    if image or url != '':
+        if url != '':
+            img = Image.open(requests.get(url, stream=True).raw)
+        else:
+            img = Image.open(image)
+        prmt  = {'role': 'user', 'parts':[prompt+txt, img]}
+    else:
+        prmt  = {'role': 'user', 'parts':[prompt+txt]}
+
+    append_message(prmt)
+
+    if lang == 'Español':
+      spinertxt = 'Espera un momento, estoy pensando...'
+    else:
+      spinertxt = 'Wait a moment, I am thinking...'
+    with st.spinner(spinertxt):
+        if len(prmt['parts']) > 1:
+            response = vision.generate_content(prmt['parts'],stream=True,safety_settings=[
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_LOW_AND_ABOVE",
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_LOW_AND_ABOVE",
+        },
+    ]
+)
+            response.resolve()
+        else:
+            response = st.session_state.chat.send_message(prmt['parts'][0])
+
+        try:
+          append_message({'role': 'model', 'parts':response.text})
+        except Exception as e:
+          append_message({'role': 'model', 'parts':f'{type(e).__name__}: {e}'})
+
+
+        st.rerun()
+
+
+
+#st.session_state.chat_session
