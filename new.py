5581e99d171c7506f36d44bef1fdd60e516810f1
masterOrigen <167365847+masterOrigen@users.noreply.github.com>
2024-06-14 15:40:53 -0400

    Update new.py

--- new.py
+++ new.py
@@ -1,52 +1,175 @@
 import streamlit as st
-from dotenv import load_dotenv
-from youtube_transcript_api import YouTubeTranscriptApi
-import os
+import pandas as pd
 import google.generativeai as genai
+import re
+from PIL import Image
+import requests
+import PyPDF2
 
-load_dotenv()  # Cargar todas las variables de entorno
+# CONFIGURACIÓN DE PÁGINA
+st.set_page_config(
+    page_title="Chat de Google AI",
+    page_icon="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png",
+    layout="wide",
+)
 
-# Configurar la API de Gemini
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# HEADER
+st.markdown('''
+Desarrollado por Google AI <img src="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png" width="20" height="20">
+, Streamlit y Python''', unsafe_allow_html=True)
+st.caption("Por Sergio Demis Lopez Martinez")
 
-# Prompt para la generación de resúmenes
-prompt = """You are YouTube video summarizer. You will be taking the transcript text
-and summarizing the entire video and providing the important summary in points
-within 250 words. Please provide the summary of the text given here: """
+# FUNCIÓN PARA EXTRAER INFORMACIÓN DE GRAPHVIZ
+def extract_graphviz_info(text: str) -> list[str]:
+    graphviz_info = text.split('```')
+    return [graph for graph in graphviz_info if ('graph' in graph or 'digraph' in graph) and ('{' in graph and '}' in graph)]
 
-# Obtener los detalles de la transcripción de los videos de YouTube
-def extract_transcript_details(youtube_video_url, language="es"):
-    try:
-        video_id = youtube_video_url.split("=")[1]
-        transcript_text = YouTubeTranscriptApi.get_transcript(video_id, languages=[language])
+# FUNCIÓN PARA AGREGAR UN MENSAJE A LA SESIÓN DE CHAT
+def append_message(message: dict) -> None:
+    st.session_state.chat_session.append({'user': message})
+    return
 
-        transcript = ""
-        for i in transcript_text:
-            transcript += " " + i["text"]
+# FUNCIÓN PARA CARGAR EL MODELO DE GOOGLE AI
+@st.cache_resource
+def load_model() -> genai.GenerativeModel:
+    model = genai.GenerativeModel('gemini-pro')
+    return model
 
-        return transcript
+# FUNCIÓN PARA CARGAR EL MODELO DE VISIÓN
+@st.cache_resource
+def load_modelvision() -> genai.GenerativeModel:
+    model = genai.GenerativeModel('gemini-pro-vision')
+    return model
 
-    except Exception as e:
-        st.error(f"Error al obtener la transcripción del video: {e}")
-        st.stop()
+# CONFIGURACIÓN INICIAL
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+model = load_model()
+vision = load_modelvision()
 
-# Generar el contenido utilizando Gemini Pro
-def generate_gemini_content(transcript_text, prompt):
-    model = genai.GenerativeModel("gemini-pro")
-    response = model.generate_content(prompt + transcript_text)
-    return response.text
+if 'chat' not in st.session_state:
+    st.session_state.chat = model.start_chat(history=[])
 
-st.title("YouTube Transcript to Detailed Notes Converter")
-youtube_link = st.text_input("Enter YouTube Video Link:")
+if 'chat_session' not in st.session_state:
+    st.session_state.chat_session = []
 
-if youtube_link:
-    video_id = youtube_link.split("=")[1]
-    st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
+# MENSAJE DE BIENVENIDA
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
 
-if st.button("Get Detailed Notes"):
-    transcript_text = extract_transcript_details(youtube_link)
+if 'welcome' not in st.session_state:
+    welcome = model.generate_content('¡Bienvenido a este chat! ¿En qué puedo ayudarte?')
+    welcome.resolve()
+    st.session_state.welcome = welcome
 
-    if transcript_text:
-        summary = generate_gemini_content(transcript_text, prompt)
-        st.markdown("## Detailed Notes:")
-        st.write(summary)
+    with st.chat_message('ai'):
+        st.write(st.session_state.welcome.text)
+else:
+    with st.chat_message('ai'):
+        st.write(st.session_state.welcome.text)
+
+# HISTORIAL DEL CHAT
+if len(st.session_state.chat_session) > 0:
+    for message in st.session_state.chat_session:
+        if message['user']['role'] == 'model':
+            with st.chat_message('ai'):
+                st.write(message['user']['parts'])
+                graphs = extract_graphviz_info(message['user']['parts'])
+                if len(graphs) > 0:
+                    for graph in graphs:
+                        st.graphviz_chart(graph, use_container_width=False)
+                        with st.expander("Ver texto"):
+                            st.code(graph, language='dot')
+        else:
+            with st.chat_message('user'):
+                st.write(message['user']['parts'])
+
+# MENÚ DE OPCIONES
+cols = st.columns(4)
+
+with cols[0]:
+    image_atachment = st.toggle("Adjuntar imagen", value=False, help="Activa este modo para adjuntar una imagen y que el chatbot pueda leerla")
+
+with cols[1]:
+    txt_atachment = st.toggle("Adjuntar archivo PDF", value=False, help="Activa este modo para adjuntar un archivo PDF y que el chatbot pueda leerlo")
+
+with cols[2]:
+    csv_excel_atachment = st.toggle("Adjuntar CSV o Excel", value=False, help="Activa este modo para adjuntar un archivo CSV o Excel y que el chatbot pueda leerlo")
+
+with cols[3]:
+    graphviz_mode = st.toggle("Modo graphviz", value=False, help="Activa este modo para generar un grafo con graphviz en .dot a partir de tu mensaje")
+
+# MANEJO DE ADJUNTOS
+if image_atachment:
+    image = st.file_uploader("Sube tu imagen", type=['png', 'jpg', 'jpeg'])
+    url = st.text_input("O pega la URL de tu imagen")
+else:
+    image = None
+    url = ''
+
+if txt_atachment:
+    pdf_file = st.file_uploader("Sube tu archivo PDF", type=['pdf'])
+else:
+    pdf_file = None
+
+if csv_excel_atachment:
+    csvexcelattachment = st.file_uploader("Sube tu archivo CSV o Excel", type=['csv', 'xlsx'])
+else:
+    csvexcelattachment = None
+
+# ENTRADA DE TEXTO
+prompt = st.chat_input("Escribe tu mensaje")
+
+if prompt:
+    txt = ''
+    if pdf_file:
+        pdf_reader = PyPDF4.PdfFileReader(pdf_file)
+        num_pages = pdf_reader.numPages
+        txt += f"El archivo PDF contiene {num_pages} páginas."
+        page_num = st.number_input("Ingresa el número de página que deseas extraer:", min_value=1, max_value=num_pages)
+        page = pdf_reader.getPage(page_num - 1)
+        page_text = page.extractText()
+        txt += page_text
+
+    if csvexcelattachment:
+        try:
+            df = pd.read_csv(csvexcelattachment)
+        except:
+            df = pd.read_excel(csvexcelattachment)
+        txt += '   Dataframe: \n' + str(df)
+
+    if graphviz_mode:
+        txt += '   Genera un grafo con graphviz en .dot \n'
+
+    if len(txt) > 5000:
+        txt = txt[:5000] + '...'
+    if image or url != '':
+        if url != '':
+            img = Image.open(requests.get(url, stream=True).raw)
+        else:
+            img = Image.open(image)
+        prmt = {'role': 'user', 'parts': [prompt + txt, img]}
+    else:
+        prmt = {'role': 'user', 'parts': [prompt + txt]}
+
+    append_message(prmt)
+
+    with st.spinner('Espera un momento, estoy pensando...'):
+        if len(prmt['parts']) > 1:
+            response = vision.generate_content(prmt['parts'], stream=True, safety_settings=[
+                {
+                    "category": "HARM_CATEGORY_HARASSMENT",
+                    "threshold": "BLOCK_LOW_AND_ABOVE",
+                },
+                {
+                    "category": "HARM_CATEGORY_HATE_SPEECH",
+                    "threshold": "BLOCK_LOW_AND_ABOVE",
+                },
+            ])
+            response.resolve()
+        else:
+            response = st.session_state.chat.send_message(prmt['parts'][0])
+
+        try:
+            append_message({'role': 'model', 'parts': response.text})
+        except Exception as e:
+            append_message({'role': 'model', 'parts': f'{type(e).__name__}: {e}'})
