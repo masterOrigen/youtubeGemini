a1cca3ccb0496d63541e2da0967782e9b8716828
masterOrigen <167365847+masterOrigen@users.noreply.github.com>
2024-06-14 13:28:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ if youtube_link:
     video_id = youtube_link.split("=")[1]
     st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
 
-if st.button("Get Detailed Notes"):
+if st.button("Procesar"):
     transcript_text = extract_transcript_details(youtube_link)
 
     if transcript_text:
