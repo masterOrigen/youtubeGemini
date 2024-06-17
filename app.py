8043e1c2917baa3b629988b4e31729d6cbeaa8e6
masterOrigen <167365847+masterOrigen@users.noreply.github.com>
2024-06-17 10:49:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,125 +1,277 @@
 import streamlit as st
 import pandas as pd
+import google.generativeai as genai
+import re
 from PIL import Image
 import requests
-import google.generativeai as genai
 
-# Configuración de la página
+#Je t'aime plus que les mots,
+#Plus que les sentiments,
+#Plus que la vie elle-même
+
 st.set_page_config(
     page_title="Google AI Chat",
     page_icon="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png",
-    layout="wide"
+    layout="wide",
 )
+# Path: Main.py
+#Author: Sergio Demis Lopez Martinez
+#------------------------------------------------------------
+#HEADER
+st.markdown('''
+Powered by Google AI <img src="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png" width="20" height="20">
+, Streamlit and Python''', unsafe_allow_html=True)
+st.caption("By Sergio Demis Lopez Martinez")
 
-# Carga del modelo generativo para texto y visión
-@st.cache(allow_output_mutation=True)
-def load_model(model_name):
-    return genai.GenerativeModel(model_name)
+#------------------------------------------------------------
+#LANGUAGE
+langcols = st.columns([0.2,0.8])
+with langcols[0]:
+  lang = st.selectbox('Select your language',
+  ('English', 'Español', 'Français', 'Deutsch',
+  'Italiano', 'Português', 'Polski', 'Nederlands',
+  'Русский', '日本語', '한국어', '中文', 'العربية',
+  'हिन्दी', 'Türkçe', 'Tiếng Việt', 'Bahasa Indonesia',
+  'ภาษาไทย', 'Română', 'Ελληνικά', 'Magyar', 'Čeština',
+  'Svenska', 'Norsk', 'Suomi', 'Dansk', 'हिन्दी', 'हिन्�'),index=1)
 
-model_text = load_model('gemini-pro')
-model_vision = load_model('gemini-pro-vision')
+if 'lang' not in st.session_state:
+    st.session_state.lang = lang
+st.divider()
 
-# Inicialización de la sesión de chat
+#------------------------------------------------------------
+#FUNCTIONS
+def extract_graphviz_info(text: str) -> list[str]:
+  """
+  The function `extract_graphviz_info` takes in a text and returns a list of graphviz code blocks found in the text.
+
+  :param text: The `text` parameter is a string that contains the text from which you want to extract Graphviz information
+  :return: a list of strings that contain either the word "graph" or "digraph". These strings are extracted from the input
+  text.
+  """
+
+  graphviz_info  = text.split('```')
+
+  return [graph for graph in graphviz_info if ('graph' in graph or 'digraph' in graph) and ('{' in graph and '}' in graph)]
+
+def append_message(message: dict) -> None:
+    """
+    The function appends a message to a chat session.
+
+    :param message: The `message` parameter is a dictionary that represents a chat message. It typically contains
+    information such as the user who sent the message and the content of the message
+    :type message: dict
+    :return: The function is not returning anything.
+    """
+    st.session_state.chat_session.append({'user': message})
+    return
+
+@st.cache_resource
+def load_model() -> genai.GenerativeModel:
+    """
+    The function `load_model()` returns an instance of the `genai.GenerativeModel` class initialized with the model name
+    'gemini-pro'.
+    :return: an instance of the `genai.GenerativeModel` class.
+    """
+    model = genai.GenerativeModel('gemini-pro')
+    return model
+
+@st.cache_resource
+def load_modelvision() -> genai.GenerativeModel:
+    """
+    The function `load_modelvision` loads a generative model for vision tasks using the `gemini-pro-vision` model.
+    :return: an instance of the `genai.GenerativeModel` class.
+    """
+    model = genai.GenerativeModel('gemini-pro-vision')
+    return model
+
+
+
+#------------------------------------------------------------
+#CONFIGURATION
+genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
+
+model = load_model()
+
+vision = load_modelvision()
+
+if 'chat' not in st.session_state:
+    st.session_state.chat = model.start_chat(history=[])
+
 if 'chat_session' not in st.session_state:
     st.session_state.chat_session = []
 
-# Función para añadir mensajes a la sesión de chat
-def append_message(message):
-    st.session_state.chat_session.append(message)
+#st.session_state.chat_session
 
-# Función para extraer bloques de Graphviz de un texto
-def extract_graphviz_info(text):
-    graphviz_info = text.split('```')
-    return [graph for graph in graphviz_info if ('graph' in graph or 'digraph' in graph) and ('{' in graph and '}' in graph)]
+#------------------------------------------------------------
+#CHAT
 
-# Saludo inicial al usuario
-if 'welcome_message' not in st.session_state:
-    st.session_state.welcome_message = model_text.generate_content('''
-    Da un saludo de bienvenida al usuario y sugiere qué puede hacer.
-    Eres un chatbot en una aplicación de chat creada en Streamlit y Python.
-    ''')
-    st.session_state.welcome_message.resolve()
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
 
-    st.write(st.session_state.welcome_message.text)
+if 'welcome' not in st.session_state or lang != st.session_state.lang:
+    st.session_state.lang = lang
+    welcome  = model.generate_content(f'''
+    Da un saludo de bienvenida al usuario y sugiere que puede hacer
+    (Puedes describir imágenes, responder preguntas, leer archivos texto, leer tablas,generar gráficos con graphviz, etc)
+    eres un chatbot en una aplicación de chat creada en streamlit y python. generate the answer in {lang}''')
+    welcome.resolve()
+    st.session_state.welcome = welcome
 
-# Columnas para opciones de adjuntos y modo Graphviz
-cols = st.columns(4)
-lang = 'Español'  # Puedes cambiar el idioma según el contexto de la aplicación
+    with st.chat_message('ai'):
+        st.write(st.session_state.welcome.text)
+else:
+    with st.chat_message('ai'):
+        st.write(st.session_state.welcome.text)
+
+if len(st.session_state.chat_session) > 0:
+    count = 0
+    for message in st.session_state.chat_session:
+
+        if message['user']['role'] == 'model':
+            with st.chat_message('ai'):
+                st.write(message['user']['parts'])
+                graphs = extract_graphviz_info(message['user']['parts'])
+                if len(graphs) > 0:
+                    for graph in graphs:
+                        st.graphviz_chart(graph,use_container_width=False)
+                        if lang == 'Español':
+                          view = "Ver texto"
+                        else:
+                          view = "View text"
+                        with st.expander(view):
+                          st.code(graph, language='dot')
+        else:
+            with st.chat_message('user'):
+                st.write(message['user']['parts'][0])
+                if len(message['user']['parts']) > 1:
+                    st.image(message['user']['parts'][1], width=200)
+        count += 1
+
+
+
+#st.session_state.chat.history
+
+cols=st.columns(4)
 
 with cols[0]:
-    image_attachment = st.toggle("Adjuntar imagen", value=False, help="Activa este modo para adjuntar una imagen y que el chatbot pueda leerla")
+    if lang == 'Español':
+      image_atachment = st.toggle("Adjuntar imagen", value=False, help="Activa este modo para adjuntar una imagen y que el chatbot pueda leerla")
+    else:
+      image_atachment = st.toggle("Attach image", value=False, help="Activate this mode to attach an image and let the chatbot read it")
 
 with cols[1]:
-    text_attachment = st.toggle("Adjuntar archivo de texto", value=False, help="Activa este modo para adjuntar un archivo de texto y que el chatbot pueda leerlo")
-
+    if lang == 'Español':
+      txt_atachment = st.toggle("Adjuntar archivo de texto", value=False, help="Activa este modo para adjuntar un archivo de texto y que el chatbot pueda leerlo")
+    else:
+      txt_atachment = st.toggle("Attach text file", value=False, help="Activate this mode to attach a text file and let the chatbot read it")
 with cols[2]:
-    csv_excel_attachment = st.toggle("Adjuntar CSV o Excel", value=False, help="Activa este modo para adjuntar un archivo CSV o Excel y que el chatbot pueda leerlo")
-
+    if lang == 'Español':
+      csv_excel_atachment = st.toggle("Adjuntar CSV o Excel", value=False, help="Activa este modo para adjuntar un archivo CSV o Excel y que el chatbot pueda leerlo")
+    else:
+      csv_excel_atachment = st.toggle("Attach CSV or Excel", value=False, help="Activate this mode to attach a CSV or Excel file and let the chatbot read it")
 with cols[3]:
-    graphviz_mode = st.toggle("Modo Graphviz", value=False, help="Activa este modo para generar un grafo con Graphviz en formato .dot a partir de tu mensaje")
-
-# Manejo de adjuntos y entrada de texto del usuario
-if text_attachment:
-    uploaded_file = st.file_uploader("Sube tu archivo de texto", type=['txt'])
+    if lang == 'Español':
+      graphviz_mode = st.toggle("Modo graphviz", value=False, help="Activa este modo para generar un grafo con graphviz en .dot a partir de tu mensaje")
+    else:
+      graphviz_mode = st.toggle("Graphviz mode", value=False, help="Activate this mode to generate a graph with graphviz in .dot from your message")
+if image_atachment:
+    if lang == 'Español':
+      image = st.file_uploader("Sube tu imagen", type=['png', 'jpg', 'jpeg'])
+      url = st.text_input("O pega la url de tu imagen")
+    else:
+      image = st.file_uploader("Upload your image", type=['png', 'jpg', 'jpeg'])
+      url = st.text_input("Or paste your image url")
 else:
-    uploaded_file = None
+    image = None
+    url = ''
 
-if csv_excel_attachment:
-    uploaded_data = st.file_uploader("Sube tu archivo CSV o Excel", type=['csv', 'xlsx'])
+
+
+if txt_atachment:
+    if lang == 'Español':
+      txtattachment = st.file_uploader("Sube tu archivo de texto", type=['txt'])
+    else:
+      txtattachment = st.file_uploader("Upload your text file", type=['txt'])
 else:
-    uploaded_data = None
+    txtattachment = None
 
-prompt = st.text_area("Escribe tu mensaje")
+if csv_excel_atachment:
+    if lang == 'Español':
+      csvexcelattachment = st.file_uploader("Sube tu archivo CSV o Excel", type=['csv', 'xlsx'])
+    else:
+      csvexcelattachment = st.file_uploader("Upload your CSV or Excel file", type=['csv', 'xlsx'])
+else:
+    csvexcelattachment = None
+if lang == 'Español':
+  prompt = st.chat_input("Escribe tu mensaje")
+else:
+  prompt = st.chat_input("Write your message")
 
-# Procesamiento del mensaje del usuario
-if st.button("Enviar"):
-    if prompt:
-        user_message = {'role': 'user', 'parts': [prompt]}
-        if uploaded_file:
-            text_content = uploaded_file.getvalue().decode("utf-8")
-            user_message['parts'].append(f'Archivo de texto:\n{text_content}')
+if prompt:
+    txt = ''
+    if txtattachment:
+        txt = txtattachment.getvalue().decode("utf-8")
+        if lang == 'Español':
+          txt = '   Archivo de texto: \n' + txt
+        else:
+          txt = '   Text file: \n' + txt
 
-        if uploaded_data:
-            try:
-                df = pd.read_csv(uploaded_data)
-            except Exception as e:
-                df = pd.read_excel(uploaded_data)
-            user_message['parts'].append(f'Dataframe:\n{df}')
+    if csvexcelattachment:
+        try:
+            df = pd.read_csv(csvexcelattachment)
+        except:
+            df = pd.read_excel(csvexcelattachment)
+        txt += '   Dataframe: \n' + str(df)
 
-        append_message(user_message)
+    if graphviz_mode:
+        if lang == 'Español':
+          txt += '   Genera un grafo con graphviz en .dot \n'
+        else:
+          txt += '   Generate a graph with graphviz in .dot \n'
 
-        if graphviz_mode:
-            user_message['parts'].append('Generar un grafo con Graphviz en formato .dot')
+    if len(txt) > 5000:
+        txt = txt[:5000] + '...'
+    if image or url != '':
+        if url != '':
+            img = Image.open(requests.get(url, stream=True).raw)
+        else:
+            img = Image.open(image)
+        prmt  = {'role': 'user', 'parts':[prompt+txt, img]}
+    else:
+        prmt  = {'role': 'user', 'parts':[prompt+txt]}
 
-        # Respuesta del modelo generativo
-        with st.spinner("Espera un momento, estoy pensando..."):
-            if image_attachment:
-                image = st.file_uploader("Sube tu imagen", type=['png', 'jpg', 'jpeg'])
-                if image:
-                    img = Image.open(image)
-                    user_message['parts'].append(img)
+    append_message(prmt)
 
-            # Uso del modelo de visión o texto según el contexto
-            if any(isinstance(part, Image.Image) for part in user_message['parts']):
-                response = model_vision.generate_content(user_message['parts'])
-            else:
-                response = model_text.send_message(user_message['parts'][0])
-
+    if lang == 'Español':
+      spinertxt = 'Espera un momento, estoy pensando...'
+    else:
+      spinertxt = 'Wait a moment, I am thinking...'
+    with st.spinner(spinertxt):
+        if len(prmt['parts']) > 1:
+            response = vision.generate_content(prmt['parts'],stream=True,safety_settings=[
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_LOW_AND_ABOVE",
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_LOW_AND_ABOVE",
+        },
+    ]
+)
             response.resolve()
+        else:
+            response = st.session_state.chat.send_message(prmt['parts'][0])
 
-            append_message({'role': 'model', 'parts': response.text})
+        try:
+          append_message({'role': 'model', 'parts':response.text})
+        except Exception as e:
+          append_message({'role': 'model', 'parts':f'{type(e).__name__}: {e}'})
 
-    st.experimental_rerun()
 
-# Mostrar el historial del chat
-if st.session_state.chat_session:
-    for message in st.session_state.chat_session:
-        if message['role'] == 'user':
-            st.write("Usuario:", message['parts'][0])
-            if len(message['parts']) > 1 and isinstance(message['parts'][1], Image.Image):
-                st.image(message['parts'][1], caption="Imagen adjunta", use_column_width=True)
-        elif message['role'] == 'model':
-            st.write("AI:", message['parts'])
+        st.rerun()
 
-# Guardar el estado de la sesión
-st.session_state.sync()
+
+
+#st.session_state.chat_session
